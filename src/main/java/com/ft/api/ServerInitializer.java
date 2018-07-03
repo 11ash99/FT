12e2d9cebaaf96681584cd499ba3a0c2fffc2ff1
11ash99 <ash.darkstar99@gmail.com>
2018-07-03 15:57:26 +0530

    Update ServerInitializer.java

--- src/main/java/com/ft/api/ServerInitializer.java
+++ src/main/java/com/ft/api/ServerInitializer.java
@@ -1,23 +1,17 @@
 package com.ft.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 
-import com.ft.api.service.FTService;
-
 @Component
 public class ServerInitializer implements ApplicationRunner {
-	
-	@Autowired
-    FTService serviceImpl;
 	
 	@Override
 	public void run(ApplicationArguments applicationArguments) throws Exception {
 		
-		serviceImpl.publishData();
+		System.out.println("Server started");
 		
     }
 
